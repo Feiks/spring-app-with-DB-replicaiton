4f42558b9acf668e97293172c0647170556fbd11
Feliks <feliks_bekeshov@mail.ru>
2024-03-22 11:23:05 +0600

    first commit

---
+++ src/main/java/com/example/demo/ProjectWithDbReplicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithDbReplicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithDbReplicasApplication.class, args);
+	}
+
+}
